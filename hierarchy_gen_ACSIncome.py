e6e8c36a61a72d85264107f81847543f0f498e62
Akram <32540529+Akram275@users.noreply.github.com>
2024-12-10 09:58:01 +0100

    Update hierarchy_gen_ACSIncome.py

--- hierarchy_gen_ACSIncome.py
+++ hierarchy_gen_ACSIncome.py
@@ -6,14 +6,14 @@ import folktables
 from folktables import ACSDataSource, ACSEmployment, ACSIncome, ACSHealthInsurance
 
 WAOB = {
-    "1": 1,
-    "2": 0,
-    "3": 0,
-    "4": 0,
-    "5": 0,
-    "6": 0,
-    "7": 0,
-    "8": 0,
+    1.0: 1,
+    2.0: 0,
+    3.0: 0,
+    4.0: 0,
+    5.0: 0,
+    6.0: 0,
+    7.0: 0,
+    8.0: 0,
 }
 
 
